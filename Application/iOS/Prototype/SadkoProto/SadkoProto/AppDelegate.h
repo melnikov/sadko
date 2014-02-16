deca18ed2d16b3874d3aeae8de7bf7ac29dc652a
Artyom Syrov <artyom.syrov@gmail.com>
2014-02-17 01:31:48 +0400

    Initial project creation

---
+++ Application/iOS/Prototype/SadkoProto/SadkoProto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SadkoProto
+//
+//  Created by Artyom Syrov on 17.02.14.
+//  Copyright (c) 2014 Stex Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Application/iOS/Prototype/SadkoProto/SadkoProtoTests/SadkoProtoTests.m
@@ -0,0 +1,34 @@
+//
+//  SadkoProtoTests.m
+//  SadkoProtoTests
+//
+//  Created by Artyom Syrov on 17.02.14.
+//  Copyright (c) 2014 Stex Group. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SadkoProtoTests : XCTestCase
+
+@end
+
+@implementation SadkoProtoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
